f7e1398a81b18957f0c46b2bf7eb620728de99dc
harutyama <128022973+harutyama@users.noreply.github.com>
2024-02-16 21:05:12 +0900

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,10 @@
+from itertools import count
+
+
+count = 80
+if count >= 90:
+  print('よくできました')
+if count >= 60:
+  print('まあまあだな')
+else:
+  print('雑魚め')
---
+++ Helloworld.js
@@ -0,0 +1,13 @@
+document.getElementById("changeColorBtn").addEventListener("click", function() {
+    let randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+});
+
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
